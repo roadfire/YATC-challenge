69c0c5cd26ff4b91fce86a59b76960ba732427cf
Vinny Carpenter <vscarpenter@gmail.com>
2014-06-16 20:50:55 -0500

    Initial revision
    Signed-off-by: Vinny Carpenter <vscarpenter@gmail.com>

---
+++ YATC-challenge/VINViewController.h
@@ -0,0 +1,13 @@
+//
+//  VINViewController.h
+//  YATC-challenge
+//
+//  Created by Vinny Carpenter on 6/16/14.
+//  Copyright (c) 2014 Vinny Carpenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VINViewController : UIViewController
+
+@end
---
+++ YATC-challenge/VINViewController.m
@@ -0,0 +1,29 @@
+//
+//  VINViewController.m
+//  YATC-challenge
+//
+//  Created by Vinny Carpenter on 6/16/14.
+//  Copyright (c) 2014 Vinny Carpenter. All rights reserved.
+//
+
+#import "VINViewController.h"
+
+@interface VINViewController ()
+
+@end
+
+@implementation VINViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
